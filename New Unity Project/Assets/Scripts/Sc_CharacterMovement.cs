0de298a2ceda4f9321654f55cc3d58ea6e6abbda
Mr-Business-Industires <51212531+Mr-Business-Industires@users.noreply.github.com>
2020-06-02 17:08:50 +0100

    player controller

---
+++ New Unity Project/Assets/Scripts/Sc_CharacterMovement.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sc_CharacterMovement : MonoBehaviour
+{
+    Rigidbody2D body;
+
+    float horizontal;
+    float vertical;
+    float moveLimiter = 0.7f;
+    float runSpeed = 20.0f;
+    public float smlSpeed = 30.0f;
+    public float medSpeed = 30.0f;
+    public float bigSpeed = 30.0f;
+
+    void Start()
+    {
+        body = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        horizontal = Input.GetAxisRaw("Horizontal");
+        vertical = Input.GetAxisRaw("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        if (horizontal != 0 && vertical != 0) // Check for diagonal movement
+        {
+            // limit movement speed diagonally, so you move at 70% speed
+            horizontal *= moveLimiter;
+            vertical *= moveLimiter;
+        }
+        body.velocity = new Vector2(horizontal * runSpeed, vertical * runSpeed);
+     
+    }
+
+
+}
