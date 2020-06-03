cefc0b6f5e1c85f27d3eecf53903c617143e2ae5
KB203473 <KB203473@falmouth.ac.uk>
2020-06-03 16:42:07 +0100

    Added a first draft of a death timer on the enemy.

---
+++ 2D Test/Assets/Scripts/Timer.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public int timeUntilDeath = 5;
+    bool controlled = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(controlled)
+        {
+            CountDown();
+            controlled = false;
+        }
+    }
+    IEnumerator CountDown()
+    {
+        Debug.Log("Started Enemy Destruction At: " + Time.time);
+        yield return new WaitForSeconds(timeUntilDeath);
+        //SEND A COMMAND FOR THE MAIN CHARACTER TO DIE
+        Destroy(this);
+    }
+}
